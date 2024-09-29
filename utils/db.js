c05ffc0a7853940037f4e9d44d20582a44524e7d
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-29 03:51:31 +0100

    Task 1

---
+++ utils/db.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+    constructor() {
+        const host = localhost;
+        const port = 27017;
+        const database = files_manager;
+        const url = `mongodb://${host}:${port}`;
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
+        this.database = this.client.db(database);
+        this.client.connect();
+    }
+
+    isAlive() {
+        return this.client.isConnected();
+    }
+    async nbUsers() {
+        return this.client.db().collection('users').countDocuments();
+    }
+    async nbFiles() {
+        return this.client.db().collection('files').countDocuments();
+    }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
