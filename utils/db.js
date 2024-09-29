3693f6c352cc764ec7b742bffe6c732e47c1978d
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-29 04:15:37 +0100

    Task 1

--- utils/db.js
+++ utils/db.js
@@ -7,15 +7,14 @@ class DBClient {
         const database = 'files_manager';
         const url = `mongodb://${host}:${port}`;
         this.client = new MongoClient(url, { useUnifiedTopology: true });
-        this.client.connect().catch((err) => {
+        this.database = null;
+        this.connect(database).catch((err) => {
             console.error('Failed to connect to MongoDB:', err);
         });
-        this.database = this.client.db(database);
-        
     }
 
     isAlive() {
-        return this.client.isConnected();
+        return this.client && this.client.topology && this.client.topology.isConnected();
     }
     async nbUsers() {
         return this.client.db().collection('users').countDocuments();
