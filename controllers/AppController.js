61deacf0ff22d3a0a499480b4ffa22423bc4d2e6
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-30 01:52:52 +0100

    task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,19 @@
 const redisUtils = require('../utils/redis');
 const dbUtils = require('../utils/db');
 import redisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import dbClient from '../utils/db';
 
 
 export default class AppController {
     static getStatus(req, res) {
       res.status(200).json({
         redis: redisClient.isAlive(),
-        db: DBClient.isAlive()
+        db: dbClient.isAlive()
       });
     }
 
     static getStats(req, res) {
-      Promise.all([DBClient.nbUsers(), DBClient.nbFiles()])
+      Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
         .then(([usersCount, filesCount]) => {
           res.status(200).json({ users: usersCount, files: filesCount });
         })
