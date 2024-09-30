7dd107afec33c456e6a63433b2c265ee67806586
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-30 02:03:53 +0100

    task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,31 @@
-const redisUtils = require('../utils/redis');
-const dbUtils = require('../utils/db');
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 export default class AppController {
-    static getStatus(req, res) {
-      res.status(200).json({
-        redis: redisClient.isAlive(),
-        db: dbClient.isAlive()
-      });
+    static async getStatus(req, res) {
+        try {
+            const redisStatus = redisClient.isAlive();
+            const dbStatus = await dbClient.isAlive();
+            res.status(200).json({
+                redis: redisStatus,
+                db: dbStatus,
+            });
+        } catch (error) {
+            console.error('Error getting status:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     }
 
-    static getStats(req, res) {
-      Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-        .then(([usersCount, filesCount]) => {
-          res.status(200).json({ users: usersCount, files: filesCount });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json({error});
-        });
+    static async getStats(req, res) {
+        try {
+            const [usersCount, filesCount] = await Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]);
+            res.status(200).json({
+                users: usersCount,
+                files: filesCount,
+            });
+        } catch (error) {
+            console.error('Error getting stats:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     }
 }
