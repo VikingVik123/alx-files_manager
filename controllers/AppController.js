e85aa462568c3d4790d0d02c6e3f91b48e441cc0
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-09-30 01:13:54 +0100

    task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+const redisUtils = require('../utils/redis');
+const dbUtils = require('../utils/db');
+
+export default class AppController {
+    static getStatus(req, res) {
+      res.status(200).json({
+        redis: redisClient.isAlive(),
+        db: dbClient.isAlive(),
+      });
+    }
+
+    static getStats(req, res) {
+      Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+        .then(([usersCount, filesCount]) => {
+          res.status(200).json({ users: usersCount, files: filesCount });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({error});
+        });
+    }
+}
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+module.exports = router;
