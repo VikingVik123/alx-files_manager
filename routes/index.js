3b7e01845bca4ffee812007a777924b2b0c1afcc
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-10-01 22:24:11 +0100

    added files class

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+
+const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -11,7 +12,6 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postNew);
 
-
-module.exports = router;
+export default router;
